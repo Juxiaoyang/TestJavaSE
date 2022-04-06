cf235667eb14e210813541545803b3ec84c03dfb
ouyang <1251908331@qq.com>
2022-04-06 23:32:55 +0800

    JavaSE方面的代码

--- TestIO/src/ITXiaoJu_Test01/Test04.java
+++ TestIO/src/ITXiaoJu_Test01/Test04.java
@@ -1,5 +1,10 @@
 package ITXiaoJu_Test01;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * @Auther: ITXiaoJu
  * @Date: 2022/4/6 - 04 - 06 - 19:03
@@ -8,9 +13,24 @@ package ITXiaoJu_Test01;
  */
 public class Test04 {
     //这是一个main方法，是程序的入口
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //创建子类 的对象
-        System.out.println("hello world");
+        File f = new File("D:\\test.txt");
+        //把对象塞到吸管里
+        FileReader fr = new FileReader(f);
+        //一次性读5个
+        char ch[] = new char[5];
+        int len = fr.read(ch);//有效长度
+        //System.out.println(len);
+        while(len!=-1){
+            for (int i = 0; i <ch.length ; i++) {
+                System.out.println(ch[i]);
+            }
+            System.out.println(len);
+            len = fr.read(ch);//有效长度
+        }
+
+        fr.close();
     }
 }
 
