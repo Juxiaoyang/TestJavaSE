11aa11147e65b555cbd5281ab4dc6ebc49b39305
ouyang <1251908331@qq.com>
2022-04-06 19:04:50 +0800

    JavaSE方面的代码

---
+++ TestIO/src/ITXiaoJu_Test01/Test04.java
@@ -0,0 +1,16 @@
+package ITXiaoJu_Test01;
+
+/**
+ * @Auther: ITXiaoJu
+ * @Date: 2022/4/6 - 04 - 06 - 19:03
+ * @Description: ITXiaoJu_Test01
+ * @version: 1.0
+ */
+public class Test04 {
+    //这是一个main方法，是程序的入口
+    public static void main(String[] args) {
+        //创建子类 的对象
+
+    }
+}
+
